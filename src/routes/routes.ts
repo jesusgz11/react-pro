e0b1e37db35a102f587ed8a6cc85e77f314f2d89
Jesus Guzman <jesusguzman.0311@gmail.com>
2023-04-11 17:24:55 -0600

    nested routes example

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,27 +1,20 @@
 import { lazy } from 'react';
 import { Route } from '../interfaces/router-dom';
+import NoLazy from '../01-lazyload/pages/NoLazy';
 
-const LazyPage1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
-const LazyPage2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
-const LazyPage3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
+const LazyLayout = lazy(() => import('../01-lazyload/layout/LazyLayout'));
 
 export const routes: Route[] = [
   {
-    to: '/lazy1',
-    path: 'lazy1',
-    name: 'Lazy-1',
-    Component: LazyPage1,
+    to: '/lazyload/',
+    path: '/lazyload/*',
+    name: 'Dashboard Lazy',
+    Component: LazyLayout,
   },
   {
-    to: '/lazy2',
-    path: 'lazy2',
-    name: 'Lazy-2',
-    Component: LazyPage2,
-  },
-  {
-    to: '/lazy3',
-    path: 'lazy3',
-    name: 'Lazy-3',
-    Component: LazyPage3,
+    to: '/no-lazy',
+    path: 'no-lazy',
+    name: 'No Lazy',
+    Component: NoLazy,
   },
 ];
